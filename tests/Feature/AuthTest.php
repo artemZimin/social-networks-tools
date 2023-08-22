436d49a25f02c3b6fb3926cb185ac6cfc87bd4ba
azimin <azimin@team.amocrm.com>
2023-08-22 20:05:45 +0300

    test: Write feature tests to register and auth user

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,187 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Testing\TestResponse;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Send register request
+     *
+     * @param array $data
+     * @return TestResponse
+     */
+    private function registerRequest(array $data): TestResponse
+    {
+        return $this->post(route('register'), $data, [
+            'Accept' => 'application/json'
+        ]);
+    }
+
+    /**
+     * Send auth request
+     *
+     * @param array $data
+     * @return TestResponse
+     */
+    private function authRequest(array $data): TestResponse
+    {
+        return $this->post(route('auth'), $data, [
+            'Accept' => 'application/json'
+        ]);
+    }
+
+    /**
+     * Auth
+     */
+    public function test_auth(): void
+    {
+        $response = $this->registerRequest([
+            'name' => 'test',
+            'email' => 'test@test.test',
+            'password' => 'testtest',
+        ]);
+
+        $response->assertOk();
+
+        $response = $this->authRequest([
+            'email' => 'test@test.test',
+            'password' => 'testtest',
+        ]);
+
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'token'
+        ]);
+    }
+
+    /**
+     * Wrong password
+     */
+    public function test_auth_wrong_password(): void
+    {
+        $response = $this->registerRequest([
+            'name' => 'test',
+            'email' => 'test@test.test',
+            'password' => 'testtest',
+        ]);
+
+        $response->assertOk();
+
+        $response = $this->authRequest([
+            'email' => 'test@test.test',
+            'password' => 'testtest1',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'email'
+            ]
+        ]);
+    }
+
+    /**
+     * Wrong email
+     */
+    public function test_auth_wrong_email(): void
+    {
+        $response = $this->registerRequest([
+            'name' => 'test',
+            'email' => 'test@test.test',
+            'password' => 'testtest',
+        ]);
+
+        $response->assertOk();
+
+        $response = $this->authRequest([
+            'email' => 'test1@test.test',
+            'password' => 'testtest',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'email'
+            ]
+        ]);
+    }
+
+    /**
+     * Wrong email and password
+     */
+    public function test_auth_wrong_email_and_password(): void
+    {
+        $response = $this->registerRequest([
+            'name' => 'test',
+            'email' => 'test@test.test',
+            'password' => 'testtest',
+        ]);
+
+        $response->assertOk();
+
+        $response = $this->authRequest([
+            'email' => 'test1@test.test',
+            'password' => 'testtest1',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'email'
+            ]
+        ]);
+    }
+
+    /**
+     * email field empty
+     */
+    public function test_auth_empty_email(): void
+    {
+        $response = $this->authRequest([
+            'password' => 'testtest1',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'email'
+            ]
+        ]);
+    }
+
+    /**
+     * password field empty
+     */
+    public function test_auth_empty_password(): void
+    {
+        $response = $this->authRequest([
+            'email' => 'test@test.test',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'password'
+            ]
+        ]);
+    }
+}
---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,261 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Str;
+use Illuminate\Testing\TestResponse;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Send register request
+     *
+     * @param array $data
+     * @return TestResponse
+     */
+    private function registerRequest(array $data): TestResponse
+    {
+        return $this->post(route('register'), $data, [
+            'Accept' => 'application/json'
+        ]);
+    }
+
+    /**
+     * Register
+     */
+    public function test_register(): void
+    {
+        $response = $this->registerRequest([
+            'password' => '123456',
+            'email' => 'test@test.test',
+            'name' => 'test',
+        ]);
+
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'name',
+            'email',
+            'updated_at',
+            'created_at',
+            'id',
+        ]);
+    }
+
+    /**
+     * Name field empty
+     */
+    public function test_name_field_empty()
+    {
+        $response = $this->registerRequest([
+            'password' => '123456',
+            'email' => 'test1@test.test',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'name',
+            ]
+        ]);
+    }
+
+    /**
+     * name field 1 char
+     */
+    public function test_name_field_1_char()
+    {
+        $response = $this->registerRequest([
+            'password' => '123456',
+            'email' => 'test1@test.test',
+            'name' => 'a'
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'name',
+            ]
+        ]);
+    }
+
+    /**
+     * name field 51 char
+     */
+    public function test_name_field_51_char()
+    {
+        $response = $this->registerRequest([
+            'password' => '123456',
+            'email' => 'test1@test.test',
+            'name' => Str::random(51),
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'name',
+            ]
+        ]);
+    }
+
+    /**
+     * email field used
+     */
+    public function test_email_used()
+    {
+        $data = [
+            'password' => '123456',
+            'email' => 'test@test.test',
+            'name' => 'test',
+        ];
+
+        $this->registerRequest($data);
+
+        $response = $this->registerRequest($data);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'email',
+            ]
+        ]);
+    }
+
+    /**
+     * email field empty
+     */
+    public function test_email_empty()
+    {
+        $response = $this->registerRequest([
+            'password' => '123456',
+            'name' => 'test',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'email',
+            ]
+        ]);
+    }
+
+    /**
+     * email field not valid format
+     */
+    public function test_email_not_valid_format()
+    {
+        $response = $this->registerRequest([
+            'password' => '123456',
+            'email' => 'wadwawsdsad',
+            'name' => 'test',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'email',
+            ]
+        ]);
+    }
+
+    /**
+     * email field 71 char
+     */
+    public function test_email_71_char()
+    {
+        $response = $this->registerRequest([
+            'password' => '123456',
+            'email' => Str::random(71),
+            'name' => 'test',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'email',
+            ]
+        ]);
+    }
+
+    /**
+     * password field empty
+     */
+    public function test_password_empty()
+    {
+        $response = $this->registerRequest([
+            'email' => 'test@test.test',
+            'name' => 'test',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'password',
+            ]
+        ]);
+    }
+
+    /**
+     * password field empty
+     */
+    public function test_password_5_char()
+    {
+        $response = $this->registerRequest([
+            'email' => 'test@test.test',
+            'password' => Str::random(5),
+            'name' => 'test',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'password',
+            ]
+        ]);
+    }
+
+    /**
+     * password field 101 char
+     */
+    public function test_password_101_char()
+    {
+        $response = $this->registerRequest([
+            'email' => 'test@test.test',
+            'password' => Str::random(101),
+            'name' => 'test',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'password',
+            ]
+        ]);
+    }
+}
